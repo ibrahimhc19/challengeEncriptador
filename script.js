24e5b6725948b2accb0c0d1b935ab9d4d806f5d6
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2023-06-27 10:00:32 -0500

    Agregango mas codigo ECMAscript que no me está funcionando por el momento.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const textArea = document.querySelector(".paraEncriptar");
-const mensaje = document.querySelector(".paraDesencriptar");
+const paraEncriptar = document.querySelector(".paraEncriptar");
+const paraDesencriptar = document.querySelector(".paraDesencriptar");
 
 // La letra "e" es convertida para "enter"
 // La letra "i" es convertida para "imes"
@@ -8,11 +8,20 @@ const mensaje = document.querySelector(".paraDesencriptar");
 // La letra "u" es convertida para "ufat"
 
 
-function encriptar(textoEncriptado){
-  let arregloCodigo = [["e","enter"], ["i", "imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
-  textoEncriptado = textoEncriptado.toLowercase();
+function encriptar(textoEncriptado) {
+    let arregloCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    textoEncriptado = textoEncriptado.toLowercase();
 
-  for (let i = o; i < arregloCodigo.length; i++){
-      
-  }
+    for (let i = o; i < arregloCodigo.length; i++) {
+        if (textoEncriptado.includes(arregloCodigo[i][0])) {
+            textoEncriptado = textoEncriptado.replaceAll(arregloCodigo[i][0], arregloCodigo[i][1]);
+        }
+    }
+    return textoEncriptado;
+}
+
+function botonEncriptar() {
+    const textoEncriptado = encriptar(paraEncriptar.value);
+    paraDesencriptar.value = textoEncriptado;
+
 }
