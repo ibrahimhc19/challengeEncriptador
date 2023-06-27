6da1240031c4f277a1d622b3ac99fb5bf1ade676
Ibrahim Calzadilla <ibrahimhc19@gmail.com>
2023-06-26 22:19:26 -0500

    Comenzando a aplicar EcmaScript.

---
+++ script.js
@@ -0,0 +1,18 @@
+const textArea = document.querySelector(".paraEncriptar");
+const mensaje = document.querySelector(".paraDesencriptar");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+
+function encriptar(textoEncriptado){
+  let arregloCodigo = [["e","enter"], ["i", "imes"], ["a","ai"], ["o","ober"], ["u","ufat"]];
+  textoEncriptado = textoEncriptado.toLowercase();
+
+  for (let i = o; i < arregloCodigo.length; i++){
+      
+  }
+}
